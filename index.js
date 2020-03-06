8f7a7327f950e2385e3c0162a1ea96141094f839
kuteshiki <vmrelaxed@yahoo.com>
2020-03-06 23:02:53 +0200

    Uncommented push!

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 let termitext = document.getElementById("termi");
 let termi = {}
 termi.shell = "$ ",
-    termi.motd = `Kuteshi's terminal emulator, TERM-I!`,
-    termi.setText = (raw) => {
-        termitext.innerHTML = raw
-    },
-    termi.send = (x) => {
-        termitext.innerHTML = termitext.innerHTML + x
-    },
-    termi.setMotd = () => {
-        termi.setText(`${termi.motd}<br>${termi.shell}`)
-    }
+termi.motd = `Kuteshi's terminal emulator, TERM-I!`,
+termi.setText = (raw) => {
+    termitext.innerHTML = raw
+},
+termi.send = (x) => {
+    termitext.innerHTML = termitext.innerHTML + x
+}
 
-termi.setMotd()
+termi.setText(`${termi.motd}<br>${termi.shell}`);
 
 document.addEventListener("keydown", function onEvent(event) {
+
     const args = termitext.innerHTML.split("$ ").slice(-1).join().split(" ");
     const key = event.key.toLowerCase();
 
@@ -22,17 +20,22 @@ document.addEventListener("keydown", function onEvent(event) {
         termi.send(" ")
         return true;
     }
+
     if (event.code === "Enter") {
+        
         if (args[0] === "help") {
-            termi.send(`<br>Help hasn't been programmed yet!<br>${termi.shell}`)
-            return true;
+        termi.send(`<br>Help hasn't been programmed yet!<br>${termi.shell}`)
+        return true;
         }
+
         termi.send(`<br>[DEBUG] Arguments: ${args.join(" ")}!<br>${termi.shell}`)
         return true;
     }
+
     if (event.code === "Backspace") {
         termi.setText(termitext.innerHTML.substr(0, termitext.innerHTML.length - 1));
         return true;
     }
+
     termitext.innerHTML = termitext.innerHTML + event.key
 });
