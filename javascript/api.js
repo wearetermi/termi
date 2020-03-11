4ce1797d9a3854187e38ff1f38f53d3398b8620d
kuteshiki <vmrelaxed@yahoo.com>
2020-03-11 23:14:19 +0300

    Cleaning and changing to gh-pages!

--- javascript/api.js
+++ javascript/api.js
@@ -17,7 +17,6 @@ termi.oshell = `${termi.color("lightgreen", "user@termi")}:${termi.color("#57669
 termi.shelldir = (dir) => {
 	return `${termi.color("lightgreen", "user@termi")}:${termi.color("#576690", dir)}$ <a></a>`
 };
-	termi.motd = `Kuteshi's terminal emulator, TERM-I!`,
 	termi.setText = (raw) => {
 		termitext.innerHTML = raw
 	},
