cf1828ea33cd1066b81d7b20459228ef6365b851
kuteshiki <vmrelaxed@yahoo.com>
2020-03-12 16:27:44 +0300

    Indenting and HTML commenting!

--- javascript/main.js
+++ javascript/main.js
@@ -18,6 +18,7 @@ setInterval(() => window.scrollTo(0, document.body.scrollHeight))
 
 // Listen for keypresses
 document.addEventListener("keydown", function onEvent(event) {
+	
 	// Arguments used in the code
 	const args = termitext.innerHTML.split(termi.shell).slice(-1).join().split(" ");
 
@@ -26,13 +27,16 @@ document.addEventListener("keydown", function onEvent(event) {
 		termi.send(" ")
 		return;
 	}
+
 	// If enter is pressed then
 	if (event.code === "Enter") {
+	
 		// Run command
 		if (commandHandle(args, termi)) {
 			// If exists, it'll go to here.
 			return;
 		} else {
+	
 			// But if it does not exist, it'll go to here.
 			// Check if arguments are empty, and if they are just print a new line
 			if (args.join("") === "") {
@@ -44,6 +48,7 @@ document.addEventListener("keydown", function onEvent(event) {
 			return;
 		}
 	}
+	
 	// Delete a charecter from the whole text if backspace is pressed
 	if (event.code === "Backspace") {
 		termi.setText(termitext.innerHTML.substr(0, termitext.innerHTML.length - 1));
@@ -52,12 +57,14 @@ document.addEventListener("keydown", function onEvent(event) {
 	if (event.code === "ArrowUp") {
 		historyHandle("up", termi, termitext)
 	}
+	
 	// If nothing matches, test if the key is one charecter. If it is, then print it, but if it's not, then don't.
 	let isOneChar=e=>!(e.split("").length>1);
 	if(isOneChar(event.key)) {
 		termitext.innerHTML = termitext.innerHTML + event.key
 	} else {
 		return;
+	
 	}
 });
 
